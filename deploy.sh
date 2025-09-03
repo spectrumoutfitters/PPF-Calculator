69e6b5ee4e05d17a1b4d40b34f482417657ce480
neel bhatt <neelbhatt1994@gmail.com>
2025-09-02 22:51:02 -0500

    Prepare for GitHub deployment

--- deploy.sh
+++ deploy.sh
@@ -87,7 +87,8 @@ case $choice in
         echo "Calculator will be available at: http://localhost:8000"
         echo "Press Ctrl+C to stop the server"
         echo ""
-        python3 -m http.server 8000
+        python3 -m 
+        http.server 8000
         ;;
     *)
         echo "❌ Invalid option. Please choose 1-5."
